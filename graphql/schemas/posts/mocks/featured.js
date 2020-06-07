2e92078a066370d96f2383f2636d884625efc019
romkravets <romann.kravets@gmail.com>
2020-06-07 18:30:49 +0300

    Apollo, GraphQL, NodeJS, KoaJS
    Apollo, GraphQL, NodeJS, KoaJS

---
+++ graphql/schemas/posts/mocks/featured.js
@@ -0,0 +1,32 @@
+const  moment = require('moment');
+
+module.exports = [
+   {
+      title: 'Can anyone code?wefwefwef',
+      date: moment().format('MMM DD, YYYY'),
+      categories: ['Tech Culture', 'Tech News'],
+      link: '#',
+      image: 'anyone_can_code.jpg',
+   },
+   {
+      title: 'Can anyone code?ffff',
+      date: moment().format('MMM DD, YYYY'),
+      categories: ['Tech Culture', 'Tech News'],
+      link: '#',
+      image: 'anyone_can_code.jpg',
+   },
+   {
+      title: 'Can anyone code?555',
+      date: moment().format('MMM DD, YYYY'),
+      categories: ['Tech Culture', 'Tech News'],
+      link: '#',
+      image: 'anyone_can_code.jpg',
+   },
+   {
+      title: 'Can anyone code?000',
+      date: moment().format('MMM DD, YYYY'),
+      categories: ['Tech Culture', 'Tech News'],
+      link: '#',
+      image: 'anyone_can_code.jpg',
+   },
+]
