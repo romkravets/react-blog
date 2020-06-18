204fea4fb82d2b68f9090fecb53aa626d5e994c8
romkravets <romann.kravets@gmail.com>
2020-06-18 10:44:52 +0300

    MySQL Migrations & Seeds

---
+++ graphql/database/actions/create-migration.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+let tableName
+
+try {
+   tableName = process.argv.find((arg) => arg.includs('--tableName=')
+   ).split('=')[1]
+} catch(err) {
+   return console.error('--tableName not found');
+}
+
+const fileName = new Date().getTime() + `_${tableName}.sql`
+
+fs.writeFile(path.resolve(__dirname, `../migrations/${fileName}`), '', (err) => {
+   if (err) throw new Error(err)
+   console.log('Created new migration file in migrations folder.')
+})
